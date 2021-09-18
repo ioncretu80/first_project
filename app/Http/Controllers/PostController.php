67481573335cc2b92f57407c5eae19b8fb06175e
”Ion <ioncretu80@gmail.com>
2021-09-18 17:41:13 +0200

    Lectia 12

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,33 +4,107 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use const http\Client\Curl\POSTREDIR_ALL;
 
 class PostController extends Controller
 {
     //
-    protected $table='posts';
+    protected $table = 'posts';
 
 
+    public function create()
+    {
+        $date = [
+            ['title' => 'title of post from phpstorm',
+                'content' => 'some interesting content',
+                'image' => 'imageblablabla.jpg',
+                'likes' => 20,
+                'is_published' => 1,
+            ],
+            [
+                'title' => 'title of post from phpstorm2',
+                'content' => 'some interesting content2',
+                'image' => 'imageblablabla2.jpg',
+                'likes' => 220,
+                'is_published' => 1,
+            ]];
+        foreach ($date as $item) {
+            Post::create($item);
+        }
+        dd('created');
 
+    }
 
-   public function create(){
-       $date=[
-           'title' => 'title of post from phpstorm',
-           'content' => 'some interesting content',
-           'image' => 'imageblablabla.jpg',
-           'likes' => '20',
-           'is_published' => '1',
+    public function index()
+    {
+        $posts = Post::where('is_published', 1)->get();
 
-       ];
-       Post::create($date);
-       dd('create');
-
-   }
-
-    public function index(){
-        $post =Post::find(1);
-        dd($post->title);
-
+        foreach ($posts as $post) {
+            dump($post->title);
+        }
         return "this is my posts";
+    }
+
+    public function update(){
+        $post= Post::find(6);
+
+        $post->update([
+            'title' => 'updated',
+            'content' => 'updated',
+            'image' => 'updated',
+            'likes' => 0,
+            'is_published' => 0
+
+        ]);
+        dd("updated");
+    }
+
+    public function delete(){
+        $post = Post::find(2);
+        $post->delete();
+        dd("delete post");
+    }
+
+    public function firstOrCreate(){
+        $post = POST::find(1);
+        $anotherPost=[
+            'title' => 'firstOrCreate',
+            'content' => 'firstOrCreate',
+            'image' => 'firstOrCreate',
+            'likes' => 0,
+            'is_published' => 0
+
+        ];
+        $post = Post::firstOrCreate([
+            'title' => 'firstOrCreate'
+
+
+        ],[
+            'title' => 'firstOrCreate',
+            'content' => 'firstOrCreate',
+            'image' => 'firstOrCreate',
+            'likes' => 0,
+            'is_published' => 0
+
+        ]);
+
+        dd($post->content);
+    }
+
+    public function updateOrCreate(){
+
+        $post = Post::updateOrCreate([
+            'title' => 'first21'
+
+
+        ],[
+            'title' => 'first22',
+            'content' => 'firstOrCreate',
+            'image' => 'firstOrCreate',
+            'likes' => 0,
+            'is_published' => 0
+
+        ]);
+        dd($post->title);
     }
 }
